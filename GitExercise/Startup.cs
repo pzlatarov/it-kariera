7ffd8e579adbc48526d66df8b8b459a7bbbd4fd5
Martin Marinov <donmartine2@abv.bg>
2020-02-09 11:51:35 +0200

    first commit

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -4,8 +4,24 @@ namespace GitExercise
 {
     public class Startup
     {
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain acces: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+        private const string Password = "abcd1234";
         public static void Main()
         {
+            bool isAuthorized = CheckCredentials();
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Acces denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -34,6 +50,12 @@ namespace GitExercise
                     break;
                 case "m":
                     OptionsManager.Multiply(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a, b);
                     break;
             }
 
